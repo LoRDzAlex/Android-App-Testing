5d98f04a5f2d8f63688542690548c97135e5051a
LoRDzAlex <alexander.stegmann2002@gmail.com>
2023-03-13 13:49:24 +0100

    Verschiedene Layouts erstellt und mit dem erstellen der eigenen Android app

---
+++ app/src/main/java/com/example/m335ifz2126/Layout3.java
@@ -0,0 +1,14 @@
+package com.example.m335ifz2126;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout3);
+    }
+}
---
+++ app/src/main/java/com/example/m335ifz2126/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.m335ifz2126;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String LOGTAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(LOGTAG, "App gestartet");
+    }
+}
